7be0fd4543fa341bb87ed7f127c84ecc06b7aebf
javigarzon1 <javi.garzon@hotmail.com>
2025-01-04 15:30:21 +0000

    excusa

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let rdm1 = Math.floor(Math.random() * who.length);
 let rdm2 = Math.floor(Math.random() * action.length);
 let rdm3 = Math.floor(Math.random() * what.length);
 let rdm4 = Math.floor(Math.random() * when.length);
-}
+
 
 document.querySelector("#excuse").innerHTML = first + who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
+};
